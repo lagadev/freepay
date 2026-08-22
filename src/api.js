29accd82bf4d77dd09fdf7898f14a7fd036b5f61
LagaTech <alamin12071985@gmail.com>
2026-08-22 06:44:05 +0600

    Update api.js

--- src/api.js
+++ src/api.js
@@ -14,7 +14,7 @@
 // Move SESSION_SECRET to `wrangler secret put SESSION_SECRET` in production.
 const CONFIG = {
   SITE_NAME: "FreePay",
-  SESSION_SECRET: "CHANGE_ME_TO_A_LONG_RANDOM_SECRET_BEFORE_DEPLOYING",
+  SESSION_SECRET: "A7xK9mQ2vL8pR4tN6zY1cW5hJ3sF0dG8bU2nI7eX4qP9rT6yM3kV1aZ8wC5",
   INVOICE_TTL_MINUTES: 15,
   SESSION_TTL_DAYS: 30,
   // Emails in this list get admin access automatically after a normal
@@ -35,7 +35,7 @@ const METHODS = [
 const METHOD_IDS = METHODS.map((m) => m.id);
 
 const AMOUNT_TOLERANCE = 0.5;
-const PBKDF2_ITERATIONS = 120000;
+const PBKDF2_ITERATIONS = 100000;
 
 function corsHeaders() {
   return {
